e52aa294fc2e839c70e4581848c9b13178446a4b
ShoTuPalush <131718941+ShoTuPalush@users.noreply.github.com>
2023-11-03 23:41:00 +0200

    step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,55 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { GlobalStyled } from './GlobalStyle';
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  onInput = evt => {
+    this.setState({ name: evt.target.value });
+  };
+
+  onAdd = evt => {
+    evt.preventDefault();
+    const contact = {
+      contacts: [
+        ...this.state.contacts,
+        { id: nanoid(), name: this.state.name },
+      ],
+      name: '',
+    };
+    this.setState(contact);
+  };
+
+  render() {
+    const { contacts, name } = this.state;
+    return (
+      <>
+        <GlobalStyled />
+        <h1>Phonebook</h1>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={this.onInput}
+            required
+          />
+        </label>
+        <button type="submit" onClick={this.onAdd}>
+          Add contact
+        </button>
+        <h2>Contacts</h2>
+        <ul>
+          {contacts.map(contact => (
+            <li key={contact.id}>{contact.name}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
