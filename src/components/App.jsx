8fbcca3e1c6d22dd46bc6413dc9dae28fa034114
ShoTuPalush <131718941+ShoTuPalush@users.noreply.github.com>
2023-11-04 00:16:27 +0200

    step 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,13 @@ import { Formik, Field, Form } from 'formik';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
     name: '',
     number: '',
   };
@@ -26,8 +32,16 @@ export class App extends Component {
     this.setState(contact);
   };
 
+  filter = () => {};
+
   render() {
-    const { contacts, name, number } = this.state;
+    const { contacts, name, number, filter } = this.state;
+    const visibleContacts = contacts.filter(contact => {
+      const hasContact = contact.name
+        .toLowerCase()
+        .includes(filter.toLowerCase());
+      return hasContact;
+    });
     return (
       <>
         <GlobalStyled />
@@ -68,13 +82,23 @@ export class App extends Component {
         </Formik>
 
         <h2>Contacts</h2>
-        <ul>
-          {contacts.map(contact => (
-            <li key={contact.id}>
-              {contact.name}: {contact.number}
-            </li>
-          ))}
-        </ul>
+
+        <p>Find contacts by name</p>
+        <input
+          type="text"
+          name="filter"
+          value={filter}
+          onChange={this.onInput}
+        />
+        {contacts.length > 0 && (
+          <ul>
+            {visibleContacts.map(contact => (
+              <li key={contact.id}>
+                {contact.name}: {contact.number}
+              </li>
+            ))}
+          </ul>
+        )}
       </>
     );
   }
