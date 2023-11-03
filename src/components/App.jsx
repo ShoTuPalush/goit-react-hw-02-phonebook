94b19df23243ce1614eb300ab75be9c595c1db0b
ShoTuPalush <131718941+ShoTuPalush@users.noreply.github.com>
2023-11-03 23:58:56 +0200

    step 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,52 +1,78 @@
 import { GlobalStyled } from './GlobalStyle';
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
+import { Formik, Field, Form } from 'formik';
 
 export class App extends Component {
   state = {
     contacts: [],
     name: '',
+    number: '',
   };
 
   onInput = evt => {
-    this.setState({ name: evt.target.value });
+    this.setState({ [evt.target.name]: evt.target.value });
   };
 
   onAdd = evt => {
-    evt.preventDefault();
     const contact = {
       contacts: [
         ...this.state.contacts,
-        { id: nanoid(), name: this.state.name },
+        { id: nanoid(), name: this.state.name, number: this.state.number },
       ],
       name: '',
+      number: '',
     };
     this.setState(contact);
   };
 
   render() {
-    const { contacts, name } = this.state;
+    const { contacts, name, number } = this.state;
     return (
       <>
         <GlobalStyled />
         <h1>Phonebook</h1>
-        <label>
-          Name
-          <input
-            type="text"
-            name="name"
-            value={name}
-            onChange={this.onInput}
-            required
-          />
-        </label>
-        <button type="submit" onClick={this.onAdd}>
-          Add contact
-        </button>
+
+        <Formik
+          initialValues={{
+            name: '',
+            number: '',
+          }}
+          onSubmit={() => this.onAdd()}
+        >
+          <Form>
+            <label>
+              Name
+              <Field
+                name="name"
+                type="text"
+                value={name}
+                onChange={this.onInput}
+                required
+              />
+            </label>
+
+            <label>
+              Number
+              <Field
+                name="number"
+                type="tel"
+                value={number}
+                onChange={this.onInput}
+                required
+              />
+            </label>
+
+            <button type="submit">Add contact</button>
+          </Form>
+        </Formik>
+
         <h2>Contacts</h2>
         <ul>
           {contacts.map(contact => (
-            <li key={contact.id}>{contact.name}</li>
+            <li key={contact.id}>
+              {contact.name}: {contact.number}
+            </li>
           ))}
         </ul>
       </>
